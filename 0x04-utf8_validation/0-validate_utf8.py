1981abaf7508773d0ab6a13abb3bef97883eebe3
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-11-18 11:40:39 +0300

    Correction

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,6 +3,8 @@
 Module to write a method that determines if a given data set
 represents a valid UTF-8 encoding.
 """
+
+
 def validUTF8(data):
     """Method to determine if data set represents UTF-8 encoding."""
     num_bytes = 0
