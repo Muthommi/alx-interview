0a9c344a1ed8648754af90a1c7e20db0fb43d69d
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-11-18 16:43:39 +0300

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    """Method determining if all boxes can be opened."""
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True
+    keys = [0]
+
+    while keys:
+        current_key = keys.pop(0)
+        for key in boxes[current_key]:
+            if 0 <= key < n and not opened[key]:
+                opened[key] = True
+                keys.append(key)
+
+    return all(opened)
