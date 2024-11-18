0ef3dffcd03d4710901ad31e321fd0560935332c
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-11-18 12:33:45 +0300

    Star wars characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,50 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+
+if (!movieId) {
+	console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+	process.exit(1);
+}
+
+// URL for starwars API
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/${movieId}';
+
+request(apiUrl, (error, response, body) => {
+	if (error) {
+		console.error('Error:', error);
+		return;
+	}
+
+	if (response.statusCode !== 200) {
+		console.error('Failed to fetch data. Status code', response.statusCode);
+		return;
+	}
+
+	const filmData = JSON.parse(body);
+	const characters = filmData.characters;
+
+	printCharacters(characters, 0);
+});
+
+function printCharacters(characters, index) {
+	if (index >= characters.length) {
+		return;
+	}
+
+	request(characters[index], (error, response, body) => {
+		if (error) {
+			console.error('Error', error);
+			return;
+		}
+		if (response.statusCode === 200) {
+			const characterData = JSON.parse(body);
+			console.log(characterData.name);
+		} else {
+			console.error('Failed to fetch character data. Status code:', response.statusCode);
+		}
+
+		printCharacters(characters, index + 1);
+	});
+}
